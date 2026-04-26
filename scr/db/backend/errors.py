6c3e83c34a630aaf9e4937dd63aa300ec819254c
tyapkinaekaterina <ekaterinatapkina48@gmail.com>
2026-04-26 18:41:52 +0300

    Update errors.py

--- scr/db/backend/errors.py
+++ scr/db/backend/errors.py
@@ -1,1 +1,11 @@
+class StudentTableError(Exception):
+    """Базовый класс для всех ошибок таблицы"""
+    pass
 
+class InvalidAgeError(StudentTableError):
+    """Ошибка: возраст не может быть меньше 0"""
+    pass
+
+class DuplicateIDError(StudentTableError):
+    """Ошибка: такой ID уже занят"""
+    pass
